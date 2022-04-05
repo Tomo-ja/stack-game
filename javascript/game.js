e2695c6979cedf3fc49097e3e0a86e072afc38b8
Tomo-ja <tomonao.hj@gmail.com>
2022-04-04 18:41:17 -0700

    add new block in test way

--- javascript/game.js
+++ javascript/game.js
@@ -1,5 +1,20 @@
 import blockMove from "./functions/blockMove.js";
 
-const box = document.getElementById("box")
+const testBtn = document.getElementById("btn")
+const parentTest = document.getElementById("game_region")
+
+let counter = 1
+
+testBtn.addEventListener('click', ()=>{
+	const newBlock = document.createElement("div")
+	newBlock.classList.add("box")
+	newBlock.setAttribute("id", `box-${counter}`)
+	const prevBlock = document.getElementById(`box-${counter - 1}`)
+	parentTest.insertBefore(newBlock, prevBlock)
+	counter ++
+})
+
+
+const box = document.getElementById("box-0")
 
 blockMove(box, 48, 5)
