41de60c462a6db2c5283cf1547c29d8e05646b39
Tomo-ja <tomonao.hj@gmail.com>
2022-04-06 22:43:33 -0700

    set up system of changing color of blocks

--- javascript/functions/addBlock.js
+++ javascript/functions/addBlock.js
@@ -1,15 +1,15 @@
-export default function addBlock(newId, width, prevBlock, playField){
+
+export default function addBlock(newId, blockColor, width, prevBlock, playField){
 	const newBlock = document.createElement("div")
 	newBlock.classList.add("box")
 	newBlock.style.width = `${width}px`
 	// newBlock.style.opacity = `${1 - newId/30}`
 	if (newId%2 === 0){
-		console.log("from right")
 		newBlock.style.left = "650px"
 	}else{
-		console.log("from left")
 		newBlock.style.left = "-650px"
 	}
+	newBlock.style.backgroundColor = blockColor
 	newBlock.setAttribute("id", `box-${newId}`)
 	playField.insertBefore(newBlock, prevBlock)
 	return newBlock
--- javascript/game.js
+++ javascript/game.js
@@ -1,23 +1,22 @@
 import blockMove from "./functions/blockMove.js";
 import addBlock from "./functions/addBlock.js";
 import resizeBlock from "./functions/resizeBlock.js";
+import { colorData } from "./data/color.js";
 
 const mode = localStorage.getItem("mode")
-const appMarginLeft = document.getElementById("app").getBoundingClientRect().left
 const bestScore = localStorage.getItem(`bestScore${mode}`)? parseInt(localStorage.getItem(`bestScore${mode}`)): 0
-const testBtn = document.getElementById("btn")
-const parentTest = document.getElementById("game_region")
 
 
+const appMarginLeft = document.getElementById("app").getBoundingClientRect().left
+const parentTest = document.getElementById("game_field")
 let counter = 1
 let prevBlock = document.getElementById("box-0")
 
-let range = {"leftLimit": 295, "rightLimit": 355}
-let newBlockWidth = 60
+let range = {"leftLimit": 225, "rightLimit": 425}
+let newBlockWidth = 200
 
-testBtn.addEventListener('click', ()=>{
-	game()
-})
+let colorCounter = 0
+
 
 let game = async()=>{
 	let isGameOver = await gameLoop()
@@ -26,17 +25,17 @@ let game = async()=>{
 		if (bestScore < counter-2){
 			localStorage.setItem(`bestScore${mode}`, counter -2)
 		}
-		await new Promise(resolve=>{setTimeout(resolve, 1000)})
-		window.open("../view/result.html", "self")
+		await new Promise(resolve=>{setTimeout(resolve, 500)})
+		window.open("../view/result.html", "_self")
 	}else{
 		game()
 	}
 }
 
-
 function gameLoop (){
 	return new Promise(resolve=>{
-		let currentBlock = addBlock(counter, newBlockWidth, prevBlock, parentTest)
+		let colorIndex = checkColorIndex(colorCounter)
+		let currentBlock = addBlock(counter,colorData[colorIndex], newBlockWidth, prevBlock, parentTest)
 		let fixBlockInfo = blockMove(currentBlock, counter)
 		parentTest.style.backgroundPosition = `left 0px bottom -${counter * 50}px`
 		fixBlockInfo.then(res => {
@@ -45,11 +44,11 @@ function gameLoop (){
 			prevBlock = currentBlock
 			let gameOver = showResult(range)
 			counter ++
+			colorCounter ++
 			resolve (gameOver)
 		})
 	})
 }
-
 
 const showResult = (range) => {
 	if (range){
@@ -59,4 +58,14 @@ const showResult = (range) => {
 	}
 }
 
+const checkColorIndex = (index) => {
+	if(index < colorData.length){
+		return index
+	}else if (index == colorData.length){
+		colorCounter = 0
+		return 0
+	}
+}
 
+
+game()
