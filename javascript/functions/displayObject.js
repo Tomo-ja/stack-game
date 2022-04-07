d3febfbe74fda003b1523315f1b727753e741d77
Tomo-ja <tomonao.hj@gmail.com>
2022-04-07 15:52:41 -0700

    set hard mode

--- javascript/functions/displayObject.js
+++ javascript/functions/displayObject.js
@@ -1,14 +1,36 @@
+
 export default function displayObject (app){
 	let counter = 1
+	const speedOption = [4, 6, 8, 10, 12, 14, 16, 18, 20]
+	const dropStartPosition = [10, 70, 130, 180, 220, 260, 280, 300, 320, 340, 380, 420, 480, 540, 600]
 
 	setInterval(()=>{
+		const color = Math.round(Math.random())
+		const speed = speedOption[Math.floor(Math.random() * speedOption.length)]
+		const dropPosition = dropStartPosition[Math.floor(Math.random()* dropStartPosition.length)]
 		const objectElement = document.createElement("img")
 
 		objectElement.src = `../images/icon_object_${counter}.svg`
 		objectElement.classList.add("object")
-	
+		// pick color
+		if(color === 0){
+			objectElement.classList.add("object-white")	
+		}else{
+			objectElement.classList.add("object-black")
+		}
+		//pick shape
+		if (counter === 5){
+			counter = 1
+		}else{
+			counter ++
+		}
+		// set speed
+		objectElement.style.animationDuration = `${speed}s`
+		//set position
+		objectElement.style.left = `${dropPosition}px`
+
 		app.appendChild(objectElement)
-	}, 1000)
+	}, 500)
 	
 }
 
